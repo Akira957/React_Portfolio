84e8b69861402682162bc94d4a96349b0900ead3
Robert Ramirez <metaknight757@gmail.com>
2022-03-28 15:11:40 -0500

    started the React portfolio

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="menu-btn">
+        <div class="btn-line"></div>
+        <div class="btn-line"></div>
+        <div class="btn-line"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
