fb70d1d437b66ca355a2757090ff2bb86e6b7724
Robert Ramirez <metaknight757@gmail.com>
2022-04-04 22:25:28 -0500

    changed title for HTML and added image background layout

--- react-portfolio/src/Pages/Home.js
+++ react-portfolio/src/Pages/Home.js
@@ -4,8 +4,16 @@ import { Link } from "react-router-dom";
 function Home() {
     return (
         <div className="home">
-            <h1>Home</h1>
-            <p>Hello World</p>
+            <h1 className="lg-heading">Robert <span className="text-secondary">Ramirez</span></h1>
+            <h2 className="sm-heading">Web Devloper, Programer, and Designer</h2>
+            <div className="icons">
+                <i href="https://www.linkedin.com/in/robert-s-ramirez/">
+                    <img src="https://img.icons8.com/ios-glyphs/60/000000/linkedin.png" alt="linkedin" />
+                </i>
+                <i href="https://github.com/Akira957">
+                    <img src="https://img.icons8.com/material-outlined/48/000000/github.png" alt="github" />
+                </i>
+            </div>
         </div>
     );
 }
--- react-portfolio/src/components/header.js
+++ react-portfolio/src/components/header.js
@@ -1,9 +1,38 @@
-import React from "react";
+// import React from "react";
+// import { Link, Router } from "react-router-dom";
+// import Home from "./Pages/Home";
+// import About from "./Pages/About";
+// import Projects from "./Pages/Projects";
+// import Contact from "./Pages/Contact";
 
 function Header() {
     return (
         <div id="header">
             <h2>Modern Portfolio</h2>
+            {/* <div className="menu-btn">
+                    <div className="btn-line"></div>
+                    <div className="btn-line"></div>
+                    <div className="btn-line"></div>
+                </div>
+                <nav className="menu">
+                    <div className="menu-branding">
+                        <div className="portrait"></div>
+                    </div>
+                    <ul className="menu-nav">
+                        <li className="nav-item current">
+                            <Link to={Home} className="nav-link">Home</Link>
+                        </li>
+                        <li className="nav-item current">
+                            <Link to={About} className="nav-link">About Me</Link>
+                        </li>
+                        <li className="nav-item current">
+                            <Link to={Projects} className="nav-link">Work</Link>
+                        </li>
+                        <li className="nav-item current">
+                            <Link to={Contact} className="nav-link">How to Reach Me</Link>
+                        </li>
+                    </ul>
+                </nav> */}
         </div>
     );
 }
